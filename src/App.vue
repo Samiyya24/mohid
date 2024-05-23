c01193dae92fb022602c48362e8984da7f167308
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-05-24 00:34:57 +0500

    new site full

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <script setup>
 import Header from "./components/header.vue";
-import SeedraSail from "./components/seedra_sail.vue";
-import Sell from "./components/sell.vue";
+import Watches from "./components/watches.vue";
 import Products from "./components/products.vue";
-import Product from "./components/product.vue";
-import Blog from "./components/blog.vue";
-import Helga from "./components/helga.vue";
+import Peoples from "./components/peoples.vue";
+import Sub from "./components/subscribe.vue";
 import Footer from "./components/footer.vue";
-import Slide from "./components/slide.vue";
 </script>
 
 <template>
   <Header></Header>
-  <SeedraSail></SeedraSail>
-  <Sell></Sell>
+  <Watches></Watches>
   <Products></Products>
-  <Product></Product>
-  <Blog></Blog>
-  <Slide></Slide>
-  <Helga></Helga>
+  <Peoples></Peoples>
+  <Sub></Sub>
   <Footer></Footer>
 </template>
 
--- src/components/clients.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container">
-    <div>
-      <h3>What out clients say</h3>
-    </div>
-  </div>
-</template>
